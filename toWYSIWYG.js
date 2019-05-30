8ed25dd24ec91f1fadbb3f4ba85bbc61827bde53
Colin Gutow <cgutow22@wooster.edu>
2019-05-30 17:33:34 -0500

    toWYSIWYG button fixed

---
+++ toWYSIWYG.js
@@ -0,0 +1,24 @@
+function toWYSIWYG() {
+	if(!document.getElementById('toWYSIWYGBtn')) {
+		var newselect=document.createElement('button');
+		newselect.id = 'toWYSIWYGBtn';
+		newselect.innerHTML = "toWYSIWYG";
+		newselect.onclick = function() {
+	currentcell = Jupyter.notebook.get_selected_cell();
+	alert(currentcell);
+	var input_area = inner_cell[1];
+	alert(input_area);
+	input_area.innerHTML = "Cick here to edit";
+	input_area.className += " rendered";
+	input_area.className += " WYSIWYG";
+	}
+	document.getElementById('maintoolbar-container').appendChild(newselect);
+	}
+	
+}
+toWYSIWYG();
+
+function removeWYSIWYG() {
+	var button = document.getElementById('toWYSIWYGBtn');
+	button.parentNode.removeChild(button);
+}
