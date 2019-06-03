472d8be6d0b73a09b66b6edb1673d3d717326fee
Colin Gutow <cgutow22@wooster.edu>
2019-06-03 08:20:08 -0500

    General work done to get the conversion working properly
    now to get the tinyMCE stuff working right in the proper cell type

--- toWYSIWYG.js
+++ toWYSIWYG.js
@@ -5,18 +5,30 @@ function toWYSIWYG() {
 		newselect.innerHTML = "toWYSIWYG";
 		newselect.onclick = function() {
 	currentcell = Jupyter.notebook.get_selected_cell();
-	alert(currentcell);
 	var input_area = currentcell.inner_cell[0];
-	alert(input_area);
 	input_area.innerHTML = "Cick here to edit";
 	input_area.className += " rendered";
 	input_area.className += " WYSIWYG";
+    tinymce.init({
+      selector: 'div.WYSIWYG',
+      inline: true, //inline mode covers up text above rather than pushing stuff down.
+      //width: 600,
+      //height: 300,
+      toolbar: "align format forecolor backcolor",
+      });
 	}
 	document.getElementById('maintoolbar-container').appendChild(newselect);
 	}
-	
+
 }
 toWYSIWYG();
+tinymce.init({
+  selector: 'div.WYSIWYG',
+  inline: true, //inline mode covers up text above rather than pushing stuff down.
+  //width: 600,
+  //height: 300,
+  toolbar: "align format forecolor backcolor",
+  });
 
 function removeWYSIWYG() {
 	var button = document.getElementById('toWYSIWYGBtn');
